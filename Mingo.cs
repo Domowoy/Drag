4daa492c0f4de877c6015ff01809337e1f6e7be0
DemkuM <42888910+DemkuM@users.noreply.github.com>
2018-09-19 01:13:05 +0200

    Baum Collider

--- Mingo.cs
+++ Mingo.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Mingo : MonoBehaviour {
+public class Mingo : MonoBehaviour
+{
     private Animator anim;
     public float walkSpeed;
     public float runSpeed;
@@ -11,20 +12,25 @@ public class Mingo : MonoBehaviour {
     public float jumpSpeed;
     [Range(0.01f, 1f)]
     public static Rigidbody rb;
- 
+    bool isColliding = false;
+    bool disableJump = false;
 
-    void Start () {
+    void Start()
+    {
         anim = gameObject.GetComponentInChildren<Animator>();
-        rb = GetComponent<Rigidbody>();    
-	}
+        rb = GetComponent<Rigidbody>();
+    }
 
     Vector3 previousMoveDirection;
-
-    void FixedUpdate() {
+    void FixedUpdate()
+    {
         Vector3 moveDirection = Vector3.zero;
 
         if (Input.GetKey("w"))
         {
+            if (isColliding)
+                  Floor.SpawnTrees(rb.position);
+
             anim.SetInteger("AnimPar", 1);
             moveDirection = transform.forward * walkSpeed * Input.GetAxis("Vertical");
             previousMoveDirection = moveDirection;
@@ -40,15 +46,37 @@ public class Mingo : MonoBehaviour {
             anim.SetInteger("AnimPar", 0);
         }
 
+
+        if (!disableJump)
+        {
             if (Input.GetKey("space"))
             {
-                anim.SetInteger("AnimPar", 3);
                 rb.AddForce(Vector3.up * jumpSpeed, ForceMode.VelocityChange);
+                disableJump = true;
+                StartCoroutine(ActivateJumpDelayed());
             }
 
-        rb.MoveRotation (Quaternion.Euler( 0, rb.rotation.eulerAngles.y + (Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime), 0));
+        }
+
+
+        rb.MoveRotation(Quaternion.Euler(0, rb.rotation.eulerAngles.y + (Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime), 0));
         rb.MovePosition(rb.position + moveDirection);
-	}
+
+        isColliding = false;
+
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        isColliding = true;
+    }
+
+    IEnumerator ActivateJumpDelayed()
+    {
+        yield return new WaitForSeconds(1);
+        disableJump = false;
+    }
+
 
     public static void Restart()
     {
